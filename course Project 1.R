dae423c745c9a4f3737eadc2148c6168b4018f96
Devansh Saxena <35166635+Devansh96@users.noreply.github.com>
2018-07-04 08:19:16 +0530

    Add files via upload

---
+++ course Project 1.R
@@ -0,0 +1,14 @@
+data <- read.table("./data.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+dim(data) 
+print((2075259 *9*8)/2^20)
+
+subset <- data$Date == "1/2/2007" | data$Date == "2/2/2007"
+newData <- data[subset, ]
+
+newData$Date <- as.Date(newData$Date, "%d/%m/%Y")
+dateTime <- paste(newData$Date, newData$Time)
+dateTime <- setNames(dateTime, "DateTime")
+
+newData <- newData[ ,!(names(newData) %in% c("Date","Time"))]
+newData <- cbind(dateTime, newData)
+newData$dateTime <- as.POSIXct(dateTime)
